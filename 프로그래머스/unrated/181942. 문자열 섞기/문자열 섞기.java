a1e4116b08dcb8a3f9203ec002f29b2cf3565cd5
TaeMin Kim <gimtaemin36@gmail.com>
2023-10-02 23:10:19 +0900

    [unrated] Title: 문자열 섞기, Time: 1.51 ms, Memory: 74.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String solution(String str1, String str2) {
+        String word = "";
+        
+        for (int i = 0; i < str1.length(); i++){
+            word += str1.substring(i,i+1);
+            word += str2.substring(i,i+1);
+            }
+       
+        return word;
+    }
+}
