3d1a913fbe8538c2c297f7521667ad85518e99c4
TaeMin Kim <gimtaemin36@gmail.com>
2024-01-22 21:48:26 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.09 ms, Memory: 76.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(String my_string) {
+        String[] str = my_string.split(" ");
+        int answer = Integer.valueOf(str[0]);
+        for(int i = 1; i < str.length; i++) {
+            if(str[i].equals("+")) {
+                answer = answer + Integer.valueOf(str[i+1]);
+            } else if(str[i].equals("-")) {
+                answer = answer - Integer.valueOf(str[i+1]);
+            } 
+        }
+        return answer;
+    }
+}
