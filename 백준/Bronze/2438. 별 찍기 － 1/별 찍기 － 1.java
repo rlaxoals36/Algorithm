74672658c6037a468dcd49499a2c058e59447e90
TaeMin Kim <gimtaemin36@gmail.com>
2024-02-08 15:08:46 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 316 ms, Memory: 18044 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        for(int i = 1; i <= a; i++){
+            for(int j = 0; j < i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        in.close();
+    }
+}
