dbd295a3bec02d6f908348866789dca4344acad7
TaeMin Kim <gimtaemin36@gmail.com>
2024-02-14 13:48:56 +0900

    [Bronze V] Title: 아스키 코드, Time: 208 ms, Memory: 17580 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Scanner in = new Scanner(System.in);
+        
+        int as = in.next().charAt(0);
+        in.close();
+        System.out.print(as);
+    }
+}
