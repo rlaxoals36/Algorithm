3c7e4ad70304fb38fcf971c1cbd2bec83c660439
TaeMin Kim <gimtaemin36@gmail.com>
2024-02-14 12:31:19 +0900

    [Bronze V] Title: 사칙연산, Time: 208 ms, Memory: 17708 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Scanner in = new Scanner(System.in);
+        
+        int a = in.nextInt();
+        int b = in.nextInt();
+        in.close();
+        
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+    }
+}
